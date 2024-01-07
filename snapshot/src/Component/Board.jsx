912fb46b307b67fa7152aab250c04edf02d4a000
Ameer jafar <ameerjafar123@gmail.com>
2024-01-07 18:43:46 +0530

    tic tac toe game is finished

--- snapshot/src/Component/Board.jsx
+++ snapshot/src/Component/Board.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { GameState } from "./GameState";
+
 export function Board({ gameState, setGameState }) {
   const [square, setSquare] = useState(Array(9).fill(""));
   const InternalStyle = {
@@ -14,6 +14,26 @@ export function Board({ gameState, setGameState }) {
     transition: 'background-color 0.3s ease',
     fontWeight: 'bold'
   };
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+  
+    for (const line of lines) {
+      const [a, b, c] = line;
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a]; // Returns 'X' or 'O' if there is a winner
+      }
+    }
+    return null; // Returns null if there is no winner
+  };
 
   const handleClick = (index) => {
     const newSquares = square.slice();
@@ -24,6 +44,11 @@ export function Board({ gameState, setGameState }) {
   };
   useEffect(() => {
     console.log(square);
+    let winner = calculateWinner(square);
+    if(winner) {
+      console.log(winner + " "+" is the winner of the game");
+      setSquare(Array(9).fill(null));
+    }
   }, [square]);
 
   return (
@@ -35,8 +60,7 @@ export function Board({ gameState, setGameState }) {
             </button>
             {(index + 1) % 3 === 0 && <br />}
           </React.Fragment>
-        ))}
-         <GameState square={square}></GameState>
+        ))};
     </div>
   );
 }
--- snapshot/src/Component/GameState.jsx
+++ snapshot/src/Component/GameState.jsx
@@ -1,10 +1,20 @@
-import { useEffect } from "react";
+export const calculateWinner = ({squares}) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
-export function GameState({ square }) {
-    useEffect(() => {
-        let side = true;
-        for(let i = 0; i < 9; i+=3) {
-        }
-    }, [square]);
-  return null;
-}
+  for (const line of lines) {
+    const [a, b, c] = line;
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]; // Returns 'X' or 'O' if there is a winner
+    }
+  }
+  return null; // Returns null if there is no winner
+};
